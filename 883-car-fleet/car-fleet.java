610616a7fc32dedd1fad0e66568a2de0bac37e2d
Jeevasankaran <s.jeevasankaran@gmail.com>
2026-04-22 23:36:21 +0530

    Time: 57 ms (96.31%) | Memory: 119.4 MB (8.17%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,22 +1,19 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        int n=position.length;
-        double[][] db=new double[n][2];
-        for(int i=0;i<n;i++){
-            db[i][0]=position[i];
-            db[i][1]=(double)(target-position[i])/speed[i];
+        Map<Integer,Double> map=new HashMap<>();
+        for(int i=0;i<position.length;i++){
+            map.put(position[i],(double)(target-position[i])/speed[i]);
         }
-        Arrays.sort(db,(a,b)->Double.compare(b[0],a[0]));
-
-        int fleat=0;
-        double currentMax=0;
-
-        for(int i=0;i<n;i++){
-            if(db[i][1]>currentMax){
-                fleat++;
-                currentMax=db[i][1];
+        Arrays.sort(position);
+        int fleet=0;
+        double lastTime=0;
+        for(int i=position.length-1;i>-1;i--){
+            double currentTime=map.get(position[i]);
+            if(currentTime>lastTime){
+                fleet++;
+                lastTime=currentTime;
             }
         }
-        return fleat;
+        return fleet;
     }
 }
