66c7301417c3d523ae5012b94d8d403097621313
Jeevasankaran <s.jeevasankaran@gmail.com>
2026-06-14 01:22:50 +0530

    [287] [Time Beats: 90.90%] [Memory Beats: 69.07%] - LeetPush

--- Find-the-Duplicate-Number.java
+++ Find-the-Duplicate-Number.java
@@ -1,9 +1,20 @@
 1class Solution {
 2    public int findDuplicate(int[] nums) {
-3        int[] freq=new int[nums.length];
-4        for(int x: nums) freq[x]++;
-5        int res=0;
-6        for(int i=1;i<freq.length;i++) if(freq[i]>1) res=i;
-7        return res;
-8    }
-9}
+3        int fast=nums[0];
+4        int slow=nums[0];
+5
+6        do{
+7            fast=nums[nums[fast]];
+8            slow=nums[slow];
+9        }while(fast!=slow);
+10
+11        int ptr1=nums[0];
+12        int ptr2=slow;
+13
+14        while(ptr1!=ptr2){
+15            ptr1=nums[ptr1];
+16            ptr2=nums[ptr2];
+17        }
+18        return ptr1;
+19    }
+20}
