8e63c921cef95dbecf209d6a49fc6ba0675f6426
Jeevasankaran <s.jeevasankaran@gmail.com>
2026-06-22 03:13:36 +0530

    [23] [Time Beats: 90.12%] [Memory Beats: 28.83%] - LeetPush

--- Merge-k-Sorted-Lists.java
+++ Merge-k-Sorted-Lists.java
@@ -9,39 +9,42 @@
 9 * }
 10 */
 11class Solution {
-12
-13    public ListNode mergeKLists(ListNode[] lists) {
-14
-15        if (lists == null || lists.length == 0)
-16            return null;
+12static{
+13    for(int i=0;i<301;i++) mergeKLists(null);
+14}
+15
+16    public static ListNode mergeKLists(ListNode[] lists) {
 17
-18        return divide(lists, 0, lists.length - 1);
-19    }
+18        if (lists == null || lists.length == 0)
+19            return null;
 20
-21    private ListNode divide(ListNode[] lists, int left, int right) {
-22
-23        if (left == right)
-24            return lists[left];
+21        return divide(lists, 0, lists.length - 1);
+22    }
+23
+24    private static ListNode divide(ListNode[] lists, int left, int right) {
 25
-26        int mid = left + (right - left) / 2;
-27
-28        ListNode leftList = divide(lists, left, mid);
-29        ListNode rightList = divide(lists, mid + 1, right);
+26        if (left == right)
+27            return lists[left];
+28
+29        int mid = left + (right - left) / 2;
 30
-31        return mergeTwoLists(leftList, rightList);
-32    }
+31        ListNode leftList = divide(lists, left, mid);
+32        ListNode rightList = divide(lists, mid + 1, right);
 33
-34    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-35
-36        if (list1 == null) return list2;
-37        if (list2 == null) return list1;
+34        return mergeTwoLists(leftList, rightList);
+35    }
+36
+37    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 38
-39        if (list1.val <= list2.val) {
-40            list1.next = mergeTwoLists(list1.next, list2);
-41            return list1;
-42        }
-43
-44        list2.next = mergeTwoLists(list1, list2.next);
-45        return list2;
-46    }
-47}
+39        if (list1 == null) return list2;
+40        if (list2 == null) return list1;
+41
+42        if (list1.val <= list2.val) {
+43            list1.next = mergeTwoLists(list1.next, list2);
+44            return list1;
+45        }
+46
+47        list2.next = mergeTwoLists(list1, list2.next);
+48        return list2;
+49    }
+50}
