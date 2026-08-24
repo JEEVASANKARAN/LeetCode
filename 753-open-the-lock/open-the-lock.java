db3f624e6049d34bc901b8a1851492c9579dfb1e
Jeevasankaran <s.jeevasankaran@gmail.com>
2026-08-24 23:44:26 +0530

    Time: 196 ms (7.82%) | Memory: 53.2 MB (19.79%) - LeetSync

--- 753-open-the-lock/open-the-lock.java
+++ 753-open-the-lock/open-the-lock.java
@@ -3,33 +3,35 @@ class Solution {
         Set<String> dead = new HashSet<>(Arrays.asList(deadends));
         Queue<String> q = new LinkedList<>();
         Set<String> visited = new HashSet<>();
-        String start="0000";
-        if(dead.contains(start)) return -1;
+        String start = "0000";
+        if (dead.contains(start))
+            return -1;
         q.add(start);
         visited.add(start);
-        int turns=0;
+        int turns = 0;
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int size = q.size();
 
-            while(size-- > 0){
+            while (size-- > 0) {
                 String curr = q.poll();
-                if(curr.equals(target)) return turns;
+                if (curr.equals(target))
+                    return turns;
 
-                for(int i = 0; i<4 ; i++){
-                    int digit = curr.charAt(i)-'0';
-                    int nextDigit=(digit+1)%10;
-                    int prevDigit=(digit+9)%10;
+                for (int i = 0; i < 4; i++) {
+                    int digit = curr.charAt(i) - '0';
+                    int nextDigit = (digit + 1) % 10;
+                    int prevDigit = (digit + 9) % 10;
 
-                    String next = curr.substring(0,i)+nextDigit+curr.substring(i+1);
-                    String prev = curr.substring(0,i)+prevDigit+curr.substring(i+1);
+                    String next = curr.substring(0, i) + nextDigit + curr.substring(i + 1);
+                    String prev = curr.substring(0, i) + prevDigit + curr.substring(i + 1);
 
-                    if(!dead.contains(next) && !visited.contains(next)){
+                    if (!dead.contains(next) && !visited.contains(next)) {
                         visited.add(next);
                         q.add(next);
                     }
 
-                    if(!dead.contains(prev) && !visited.contains(prev)){
+                    if (!dead.contains(prev) && !visited.contains(prev)) {
                         visited.add(prev);
                         q.add(prev);
                     }
