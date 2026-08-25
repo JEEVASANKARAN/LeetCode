2f1d5674d853061beb6e2e91f75ea7c1eb9ea7ff
Jeevasankaran <s.jeevasankaran@gmail.com>
2026-08-26 01:34:53 +0530

    Time: 4 ms (91.37%) | Memory: 46.9 MB (88.58%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,9 +1,7 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-
         boolean[] visited = new boolean[numCourses];
         boolean[] path = new boolean[numCourses];
-
         List<List<Integer>> graph = new ArrayList<>();
 
         for (int i = 0; i < numCourses; i++)
@@ -13,41 +11,27 @@ class Solution {
             graph.get(p[1]).add(p[0]);
 
         for (int i = 0; i < numCourses; i++) {
-
             if (!visited[i]) {
-
                 if (dfs(i, graph, visited, path))
                     return false;
             }
         }
-
         return true;
     }
 
-    public boolean dfs(
-            int node,
-            List<List<Integer>> graph,
-            boolean[] visited,
-            boolean[] path) {
-
+    public boolean dfs(int node, List<List<Integer>> graph, boolean[] visited, boolean[] path) {
         visited[node] = true;
         path[node] = true;
 
         for (int next : graph.get(node)) {
 
             if (!visited[next]) {
-
                 if (dfs(next, graph, visited, path))
                     return true;
-
-            } else if (path[next]) {
-
+            } else if (path[next])
                 return true;
-            }
         }
-
         path[node] = false;
-
         return false;
     }
 }
