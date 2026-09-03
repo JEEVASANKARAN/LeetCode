5ba3a8a9fb4777499ff6b8fecf3040cffc926a87
Jeevasankaran <s.jeevasankaran@gmail.com>
2026-09-03 22:32:46 +0530

    Time: 3 ms (99.81%) | Memory: 46.5 MB (42.71%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,10 +1,17 @@
 class Solution {
     public String intToRoman(int num) {
-        String ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
-        String tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
-        String hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
-        String thos[] = {"","M","MM","MMM"};
         
-        return thos[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
+        int[] values = {1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder roman = new StringBuilder();
+
+        for(int i=0; i< values.length; i++){
+            while(num>=values[i]){
+                roman.append(symbols[i]);
+                num-=values[i];
+
+            }
+        }
+        return roman.toString();
     }
 }
